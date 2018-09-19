e4d0472526714f413bcae58ec5d07912c06dfe77
tracydaniellelong <tracydaniellelong@gmail.com>
2018-09-19 14:15:39 -0400

    Print 1 to 100

--- printtoscreen.rb
+++ printtoscreen.rb
@@ -1,7 +1,5 @@
-puts "hello world"
-puts "hello again"
-puts "i like typing this"
-puts "this is fun"
-puts "Yay! Printing"
-puts "I'd much rather you 'not'."
-puts 'I "said"do not touch this'
+100.times do |x|
+	print x+1
+	
+end
+	
